3fd218285f2c6e96937475105305391e435f1db9
hofab <hofab@fabianhof.com>
2017-10-19 13:18:07 -0400

    Dotfiles update!

--- fish/functions/fa.fish
+++ fish/functions/fa.fish
@@ -1,3 +1,3 @@
 function fa --description 'Fuzzy git add'
-	git add (git status | rg -o '(\w*\.c)' | fzf --height=10);
+    git add (git status | rg -o '(\w*\.c)' | fzf --height=10);
 end
--- fish/functions/fco.fish
+++ fish/functions/fco.fish
@@ -1,3 +1,3 @@
 function fco --description 'Fuzzy-find and checkout a branch'
-	git branch --all | grep -v HEAD | string trim | fzf | xargs git checkout
+    git branch --all | grep -v HEAD | string trim | fzf --height=10 | xargs git checkout
 end
