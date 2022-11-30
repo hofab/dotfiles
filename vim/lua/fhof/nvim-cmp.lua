1d98abc6cdc77fc174c7e2419bd5ae9d607d9248
Fabian Hof <fhof@utias-sfl.net>
2022-11-30 11:01:36 -0500

    Update deprecated nvim-cmp option

--- vim/lua/fhof/nvim-cmp.lua
+++ vim/lua/fhof/nvim-cmp.lua
@@ -58,7 +58,7 @@ cmp.setup.cmdline(':', {
 })
 
 -- Setup lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 -- Add additional capabilities supported by nvim-cmp
 -- local capabilities = vim.lsp.protocol.make_client_capabilities()
