0ef6d813f2a8e158defa354030ecd7f563bf46a7
Fabian Hof <fhof@utias-sfl.net>
2023-01-03 08:22:14 -0500

    Updated single.sh for single monitor use

--- i3/single.sh
+++ i3/single.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DVI-I-2-1 --off --output DVI-I-3-2 --off --output eDP-1 --mode 2560x1600 --pos 0x0 --primary
+xrandr --output DVI-I-1-1 --off --output DVI-I-2-2 --off --output eDP-1 --mode 2560x1600 --pos 0x0 --primary
