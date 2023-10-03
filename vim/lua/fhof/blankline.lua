562fb36a4fe629b2e392daf0fa79af3139850a0c
Fabian Hof <fhof@utias-sfl.net>
2023-10-03 08:55:48 -0400

    Delete vim blankline

--- vim/lua/fhof/blankline.lua
+++
@@ -1,6 +0,0 @@
-require("indent_blankline").setup {
-    -- for example, context is off by default, use this to turn it on
-    indent_blankline_enabled = false,
-    show_current_context = false,
-    show_current_context_start = false,
-}
