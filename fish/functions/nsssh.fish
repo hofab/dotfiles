9742a17ef16e1a18e2c69e04187a640407b67a6d
Fabian Hof <fhof@utias-sfl.net>
2024-02-08 09:54:51 -0500

    Added timeout as case for nssh script
    - If we don't have a connection, ssh times out. But we didn't handle the
      case, we just assumed we have to add the host key. Going forward we
      will check the connection and only proceed if it does not time out.

--- fish/functions/nsssh.fish
+++ fish/functions/nsssh.fish
@@ -6,11 +6,17 @@ function nsssh --description 'SSH into the NorSat-flatsat'
         exit
     end
 
-    if ssh -oHostKeyAlgorithms=+ssh-rsa -o StrictHostKeyChecking=accept-new -o ConnectTimeout=5 root@$POBC_IP 'exit 0'
+    set command_output (ssh -oHostKeyAlgorithms=+ssh-rsa -o StrictHostKeyChecking=accept-new -o ConnectTimeout=3 root@$POBC_IP 'exit 0' 2>&1)
+
+    if string match --quiet --ignore-case "*Connection timed out*" $command_output
+        echo "Connection timed out"
+        return -1
+    end
+
+    if string match --quiet --ignore-case "*WARNING: REMOTE HOST IDENTIFICATION HAS CHANGED*" $command_output
+        ssh-keygen -f $HOME"/.ssh/known_hosts" -R $POBC_IP
         ssh -oHostKeyAlgorithms=+ssh-rsa -o StrictHostKeyChecking=accept-new root@$POBC_IP
     else
-        # ssh-keygen -f "/home/fhof/.ssh/known_hosts" -R $POBC_IP
-        # ssh -oHostKeyAlgorithms=+ssh-rsa -o StrictHostKeyChecking=accept-new root@$POBC_IP
-        echo "No connection"
+        ssh -oHostKeyAlgorithms=+ssh-rsa -o StrictHostKeyChecking=accept-new root@$POBC_IP
     end
 end
