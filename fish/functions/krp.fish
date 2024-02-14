22f69c63120a51796fcad005eecc575127219457
Fabian Hof <fhof@utias-sfl.net>
2024-02-14 16:59:57 -0500

    Added krp.fish which kills a process on a remote server
    - Sometimes my app locks up, and we want to quickly reload it, this is the way to do going forward

---
+++ fish/functions/krp.fish
@@ -0,0 +1,25 @@
+function krp --description 'Kill -n 9 a process on the remote host given by work-config'
+    # Check if enough arguments are provided
+    if test (count $argv) -lt 1
+        echo "Usage: krp <service_name>"
+        return 1
+    end
+
+    source central-config.fish
+
+    # set remote_host $argv[1]
+    set service_name $argv[1]
+
+    # Use SSH to execute ps command on the remote machine and find PID
+    set pid (eval $SSH_CMD $REMOTE "ps | grep '$service_name' | grep -v grep | awk '{print \$1}'")
+
+    # Check if PID is found
+    if test -n "$pid"
+        # Kill the process on the remote machine using SSH
+        eval $SSH_CMD $REMOTE "kill -9 '$pid'"
+        echo "Service '$service_name' killed on $REMOTE (PID: $pid)."
+    else
+        echo "Service '$service_name' not found on $REMOTE."
+    end
+end
+
