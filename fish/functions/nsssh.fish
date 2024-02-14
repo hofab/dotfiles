810cf072573bb959c5545585028bda76dcdf907c
Fabian Hof <fhof@utias-sfl.net>
2024-02-14 16:58:55 -0500

    Modified nsssh.fish to use newly introduce fish work config

--- fish/functions/nsssh.fish
+++ fish/functions/nsssh.fish
@@ -1,12 +1,14 @@
 function nsssh --description 'SSH into the NorSat-flatsat'
     if [ (count $argv) -gt 0 ]
-        set POBC_IP "192.168.11.$argv"
+        set POBC_IP "192.168.208.$argv"
     else
-        echo "nsssh <ip subaddress> e.g. nsssh 12 - 192.168.11.<12>"
+        echo "nsssh <ip subaddress> e.g. nsssh 12 - 192.168.208.<12>"
         exit
     end
 
-    set command_output (ssh -oHostKeyAlgorithms=+ssh-rsa -o StrictHostKeyChecking=accept-new -o ConnectTimeout=3 root@$POBC_IP 'exit 0' 2>&1)
+    source central-config.fish
+
+    set command_output (ssh -oHostKeyAlgorithms=+ssh-rsa -o StrictHostKeyChecking=accept-new -o ConnectTimeout=5 root@$POBC_IP 'exit 0' 2>&1)
 
     if string match --quiet --ignore-case "*Connection timed out*" $command_output
         echo "Connection timed out"
@@ -15,8 +17,8 @@ function nsssh --description 'SSH into the NorSat-flatsat'
 
     if string match --quiet --ignore-case "*WARNING: REMOTE HOST IDENTIFICATION HAS CHANGED*" $command_output
         ssh-keygen -f $HOME"/.ssh/known_hosts" -R $POBC_IP
-        ssh -oHostKeyAlgorithms=+ssh-rsa -o StrictHostKeyChecking=accept-new root@$POBC_IP
+        eval $SSH_CMD root@$POBC_IP
     else
-        ssh -oHostKeyAlgorithms=+ssh-rsa -o StrictHostKeyChecking=accept-new root@$POBC_IP
+        eval $SSH_CMD root@$POBC_IP
     end
 end
