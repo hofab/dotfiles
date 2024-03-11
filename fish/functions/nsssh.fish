ef91599a6a29ea44824108f4517d7552f73d5fcd
Fabian Hof <fhof@utias-sfl.net>
2024-03-11 11:24:04 -0400

    Update nsssh with config and better timeout handling

--- fish/functions/nsssh.fish
+++ fish/functions/nsssh.fish
@@ -6,9 +6,14 @@ function nsssh --description 'SSH into the NorSat-flatsat'
         exit
     end
 
-    source central-config.fish
+    source ~/.config/fish/functions/central-config.fish
 
-    set command_output (ssh -oHostKeyAlgorithms=+ssh-rsa -o StrictHostKeyChecking=accept-new -o ConnectTimeout=5 root@$POBC_IP 'exit 0' 2>&1)
+    set command $SSH_CMD
+    set -a command "-o ConnectTimeout=5 root@$POBC_IP 'exit 0' 2>&1"
+
+    set command_output (eval $command)
+
+    echo $command_output
 
     if string match --quiet --ignore-case "*Connection timed out*" $command_output
         echo "Connection timed out"
@@ -16,7 +21,7 @@ function nsssh --description 'SSH into the NorSat-flatsat'
     end
 
     if string match --quiet --ignore-case "*WARNING: REMOTE HOST IDENTIFICATION HAS CHANGED*" $command_output
-        ssh-keygen -f $HOME"/.ssh/known_hosts" -R $POBC_IP
+        ssh-keygen -f $HOME"/.ssh/known_hosts" -R $POBC_IP > /dev/null
         eval $SSH_CMD root@$POBC_IP
     else
         eval $SSH_CMD root@$POBC_IP
