9cab0ea7339b0fc5e57b4bc9f9b6b5fbd60c6316
Fabian Hof <fhof@utias-sfl.net>
2024-04-11 08:22:56 -0400

    Fixed screen setting script for triple screen

--- i3/triple.sh
+++ i3/triple.sh
@@ -2,28 +2,28 @@
 
 echo "TRI-SCRIPT"
 
-leftmon_args="--mode 3840x2160 --scale 1x1 --pos 0x0"
-midmon_args="--mode 3840x2160 --scale 1x1 --pos 3840x0 --primary"
+midmon_args="--mode 3840x2160 --scale 1x1 --pos 0x0"
+leftmon_args="--mode 3840x2160 --scale 1x1 --pos 3840x0 --primary"
 
 # leftmon_args_work="--mode 2560x1440 --scale 1x1 --pos 0x0"
 # midmon_args_work="--mode 2560x1440 --scale 1x1 --pos 2560x0 --primary"
 
 if $(xrandr -q | grep -q DVI); then
     if $(xrandr -q | grep -q "DVI-I-2-1" | grep -q "3840+0"); then
-        midmon="DVI-I-2-1"
-    else
         leftmon="DVI-I-2-1"
+    else
+        midmon="DVI-I-2-1"
     fi
     if $(xrandr -q | grep -q "DVI-I-3-2" | grep -q "0+0"); then
-        leftmon="DVI-I-3-2"
-    else
         midmon="DVI-I-3-2"
+    else
+        leftmon="DVI-I-3-2"
     fi
 fi
 
 xrandr \
-    --output $leftmon $leftmon_args \
     --output $midmon $midmon_args \
+    --output $leftmon $leftmon_args \
     --output eDP-1 --off
 
 # leftmon="DVI-I-2-1"
