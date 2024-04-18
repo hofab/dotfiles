ea5ffc0ac365babf44e5caedf4822526a493b58b
Fabian Hof <Fabian.hof@gmail.com>
2024-04-18 13:41:55 -0400

    Renamed work to work.sh so we can call it from the i3 config

--- i3/work.sh
+++ i3/work.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-echo "OFFICE-SCRIPT"
-
 leftmon_args="--mode 2560x1440 --scale 1x1 --pos 0x0"
 midmon_args="--mode 2560x1440 --scale 1x1 --pos 2560x0 --primary"
 
 if $(xrandr -q | grep -q DVI); then
-    midmon="DVI-I-3-2"
-    leftmon="DVI-I-2-1"
+    midmon="DVI-I-5-4"
+    leftmon="DVI-I-4-3"
     # midmon="DVI-I-2-1"
     # leftmon="DVI-I-3-2"
 fi
