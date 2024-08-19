2fd898b294d675614379b4559f5cecaadf2d8ece
Fabian Hof <Fabian.hof@gmail.com>
2024-08-19 14:13:01 -0400

    Make xrandr turn of all dvi outputs

--- i3/single.sh
+++ i3/single.sh
@@ -3,6 +3,6 @@
 #
 primary_args="--mode 3840x2160 --scale 1x1 --pos 0x0"
 
-xrandr --output DVI-I-3-2 --off --output DVI-I-2-1 --off --output eDP-1 --mode 2560x1600 --pos 0x0 --primary
+xrandr --output DVI-I-4-4 --off --output DVI-I-3-3 --off --output DVI-I-2-2 --off --output DVI-I-1-1 --off --output eDP-1 --mode 2560x1600 --pos 0x0 --primary
 
 # xrandr --output DVI-I-2-1 $primary_args --primary --output DVI-I-3-2 --off --output eDP-1 --off
