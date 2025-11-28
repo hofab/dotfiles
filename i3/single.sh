cfa74269bc615190a3a0d7b45b9adadd281147a1
Fabian Hof <Fabian.hof@gmail.com>
2025-11-28 16:23:06 -0500

    Update monitor scripts with EDID info
    - While hotplugging my docking station, the dvi outputs changed randomly
      resulting in my outputs to be switched between the monitors.
    - Using the EDID information, this problem should now be fixed.
    - But it can happen that i3 still messes with the output when the output
      names change, but for now it seems to be fixed.

--- i3/single.sh
+++ i3/single.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
 source ~/.config/i3/mon_config
+source ~/.config/i3/get_edid.sh
 
 primary_args="--mode 3840x2160 --scale 1x1 --pos 0x0"
 
-xrandr --output ${LEFT_MON} --off --output ${MID_MON} --off --output eDP-1 --mode 2560x1600 --pos 0x0 --primary
+echo "TESTING" >> /tmp/log.log
+echo "$LEFT_MON" >> /tmp/log.log
+echo "$RIGHT_MON" >> /tmp/log.log
+
+xrandr --output ${LEFT_MON} --off \
+    --output ${RIGHT_MON} --off \
+    --output eDP-1 --mode 2560x1600 --pos 0x0 --primary
 
 # --output DVI-I-2-2 --off --output DVI-I-1-1 --off
 
--- i3/triple.sh
+++ i3/triple.sh
@@ -3,31 +3,13 @@
 echo "TRI-SCRIPT"
 
 . ~/.config/i3/mon_config
+source ~/.config/i3/get_edid.sh
 
-if [[ ! -n ${LEFT_MON} ]]; then
-    if [[ ! -n ${MID_MON} ]]; then
-        exit 0
-    fi
-fi
-
-if $(xrandr -q | grep -q DVI); then
-    if $(xrandr -q | grep -q ${LEFT_MON} | grep -q "3840+0"); then
-        midmon=${LEFT_MON}
-    else
-        leftmon=${LEFT_MON}
-    fi
-    if $(xrandr -q | grep -q ${MID_MON} | grep -q "0+0"); then
-        leftmon=${MID_MON}
-    else
-        midmon=${MID_MON}
-    fi
-fi
-
-if [[ ! -z ${midmon} ]]; then
-    if [[ ! -z ${leftmon} ]]; then
+if [[ ! -z ${RIGHT_MON} ]]; then
+    if [[ ! -z ${LEFT_MON} ]]; then
         xrandr \
-            --output $midmon $MID_MON_ARGS \
-            --output $leftmon $LEFT_MON_ARGS \
+            --output $RIGHT_MON $RIGHT_MON_ARGS \
+            --output $LEFT_MON $LEFT_MON_ARGS \
             --output eDP-1 --off
     fi
 fi
