24df1277c5a9a620024b2069bbe189edb6480c86
Fabian Hof <Fabian.hof@gmail.com>
2026-01-09 13:21:01 -0500

    fixup! Update treesitter init due to api change

--- vim/lua/fhof/treesitter.lua
+++ vim/lua/fhof/treesitter.lua
@@ -1,13 +1,10 @@
 vim.api.nvim_create_autocmd('FileType', {
-    pattern = { 'cpp', 'c', 'python', 'markdown', 'bash', 'fish' },
+    pattern = { 'cpp', 'c', 'python', 'markdown', 'sh', 'fish' },
     callback = function()
         vim.treesitter.start()
         -- Treesitter folding
         -- vim.wo.foldmethod = 'expr'
         -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-
-        -- Treesitter indentation
-        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
     end,
 })
 
