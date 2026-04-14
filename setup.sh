bb24e4f9739e17344919b5bf73fee9df6ec64c4a
Fabian Hof <Fabian.hof@gmail.com>
2026-04-14 14:24:22 -0400

    Draft of my setup script to create all the symlinks

---
+++ setup.sh
@@ -0,0 +1,26 @@
+ln -sf ~/dotfiles/config/fish ~/.config/fish
+
+ln -sf ~/dotfiles/config/startship/starship.toml ~/.config/starship.toml
+
+ln -sf ~/dotfiles/config/rofi ~/.config/rofi
+
+ln -sf ~/dotfiles/config/display ~/.config/display
+
+ln -sf ~/dotfiles/config/alacritty ~/.config/alacritty
+
+ln -sf ~/dotfiles/config/tmuxinator ~/.config/tmuxinator
+ln -sf ~/dotfiles/config/tmux ~/.config/tmux
+ln -sf ~/dotfiles/config/tmux.conf ~/.tmux.conf
+
+ln -sf ~/dotfiles/config/nvim ~/.config/nvim
+
+# setup python environment for nvim
+curl -LsSf https://astral.sh/uv/install.sh | sh
+source $HOME/.local/bin/env.fish
+uv tool install --upgrade pynvim
+
+# open neovim
+:PlugInstall
+:TSUpdate # if needed
+
+
