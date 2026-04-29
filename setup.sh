c73077c3a32a349428ce7f78199f328b0659ce2a
Fabian Hof <Fabian.hof@gmail.com>
2026-04-29 09:09:43 -0400

    Update setup.sh script

--- setup.sh
+++ setup.sh
@@ -1,18 +1,20 @@
+ln -sf ~/dotfiles/config/alacritty ~/.config/alacritty
+
+ln -sf ~/dotfiles/config/tmux ~/.config/tmux
+ln -sf ~/dotfiles/config/tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/config/tmuxinator ~/.config/tmuxinator
+
 ln -sf ~/dotfiles/config/fish ~/.config/fish
 
 ln -sf ~/dotfiles/config/startship/starship.toml ~/.config/starship.toml
+
+ln -sf ~/dotfiles/config/nvim ~/.config/nvim
 
 ln -sf ~/dotfiles/config/rofi ~/.config/rofi
 
 ln -sf ~/dotfiles/config/display ~/.config/display
 
-ln -sf ~/dotfiles/config/alacritty ~/.config/alacritty
-
-ln -sf ~/dotfiles/config/tmuxinator ~/.config/tmuxinator
-ln -sf ~/dotfiles/config/tmux ~/.config/tmux
-ln -sf ~/dotfiles/config/tmux.conf ~/.tmux.conf
-
-ln -sf ~/dotfiles/config/nvim ~/.config/nvim
+ln -sf ~/dotfiles/config/eza ~/.config/eza
 
 ln -sf ~/dotfiles/scripts/mount_network_drive.sh ~/.local/bin/
 
